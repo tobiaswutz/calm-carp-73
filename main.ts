0d60b837c557955a95c2113c0f0e1eb90f718bc7
Tobias <tobiaswutz22@gmail.com>
2023-09-08 22:49:41 +0200

    changes

--- main.ts
+++ main.ts
@@ -1,31 +1,27 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { User } from "./types.ts";
+const db = await Deno.openKv("./db/database.ts");
 
 const router = new Router();
 router
-  .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+  .post("/create", async (context) => {
+    const body = context.request.body();
+    const user: User = await body.value;
+    user.id = crypto.randomUUID();
+    const usersFromDB: User[] | null = (await db.get<User[]>(['users'])).value;
+    if (!usersFromDB) { await db.set(['users'], [user]); } else { await db.set(['users'], [...usersFromDB, user]); }
+    context.response.body = await db.get(['users']);
   })
-  .get("/api", (context) => {
-    context.response.body = data;
+  .get("/read", async (context) => {
+    context.response.body = new TextEncoder().encode(JSON.stringify(await db.get(['users'])));
   })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
-  });
+
 
 const app = new Application();
-app.use(oakCors()); // Enable CORS for All Routes
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
-
+console.log("Dein Server ist jetzt unter http://localhost:8000/ erreichbar.");
 await app.listen({ port: 8000 });
+
---
+++ types.ts
@@ -0,0 +1,7 @@
+export interface User {
+  name: string;
+  age: number;
+  height: number;
+  weight: number;
+  id?: string;
+}
